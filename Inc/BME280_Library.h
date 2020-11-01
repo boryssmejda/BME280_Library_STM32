d074cc0c28193a0397e25ac7e5eb89d99a885419
boryssmejda <borys.smejda@gmail.com>
2020-11-01 23:04:18 +0100

    Register mapping in progress.

--- Inc/BME280_Library.h
+++ Inc/BME280_Library.h
@@ -1,14 +1,140 @@
 #ifndef _BME280_LIBRARY__
 #define _BME280_LIBRARY__
 
-typedef struct bme280_handler_t bme280_handler_t;
+enum BME280_STATUS
+{
+	BME280_OK,
+	BME280_FAIL
+};
 
-#define BME280_DEVICE_ADDRESS (0x76<<1)
-#define BME280_CHIP_ID_REGISTER_ADDRESS 0xD0
-#define BME280_CHIP_ID_VALUE 0x60 // to check if everything functions correctly
-#define BME280_RESET_REGISTER_ADDRESS 0xE0
+struct bme280_register_frame
+{
+	uint8_t registerAddress;
+	uint8_t registerData;
+};
+
+struct bme280_register_ctrl_hum
+{
+	uint8_t reserved: 	5; // do not touch this register
+	uint8_t osrs_h:		3;
+};
+
+struct bme280_register_status
+{
+	uint8_t reserved_from_bit_7_to_4: 	4;
+	uint8_t measuring: 					1;
+	uint8_t reserved_from_bit_2_to_1:	2;
+	uint8_t im_update:					1;
+};
+
+struct bme280_register_ctrl_meas
+{
+	uint8_t osrs_t:	3;
+	uint8_t osrs_p: 3;
+	uint8_t mode:	2;
+};
+
+struct bme280_register_config
+{
+	uint8_t t_sb: 			3;
+	uint8_t filter: 		3;
+	uint8_t reserved_bit_1: 1;
+	uint8_t spi3w_en: 		1;
+};
+
+struct bme280_register_press_msb
+{
+	uint8_t press_msb;
+};
+
+struct bme280_register_press_lsb
+{
+	uint8_t press_lsb;
+};
+
+struct bme280_register_press_xlsb
+{
+	uint8_t press_xlsb: 				4;
+	uint8_t reserved_from_bit_3_to_0: 	4;
+};
+
+struct bme280_register_temp_msb
+{
+	uint8_t temp_msb;
+};
+
+struct bme280_register_temp_lsb
+{
+	uint8_t temp_lsb;
+};
+
+struct bme280_register_temp_xlsb
+{
+	uint8_t temp_xlsb: 				  	4;
+	uint8_t reserved_from_bit_3_to_0: 	4;
+};
+
+struct bme280_register_hum_msb
+{
+	uint8_t hum_msb;
+};
+
+struct bme280_register_hum_lsb
+{
+	uint8_t hum_lsb;
+};
+
+typedef enum BME280_STATUS BME280_STATUS;
+typedef struct bme280_handler_t bme280_handler_t;
+typedef struct bme280_register_frame bme280_register_frame;
+
+#define DEFAULT_TIMEOUT 				100
+#define BME280_RW_READ 					1
+#define BME280_RW_WRITE 				0
+#define BME280_DEVICE_ADDRESS 			(0x76<<1)
+#define BME280_CHIP_ID_VALUE 			0x60 // to check if everything functions correctly
+#define BME280_RESET_REGISTER_VALUE 	0xB6
+
+
+// Table 21 Register 0xF3 “status”
+#define BME280_STATUS_MEASURING_RUNNING					1
+#define BME280_STATUS_MEASURING_TRANSFERRED 			0
+#define BME280_STATUS_IM_UPDATE_COPYING_IN_PROGRESS		1
+#define BME280_STATUS_IM_UPDATE_COPYING_DONE			0
+
+// Table 25  Register Settings Mode
+#define BME280_OPERATION_MODE_SLEEP		0
+#define BME280_OPERATION_MODE_FORCED_1	1
+#define BME280_OPERATION_MODE_FORCED_2	2
+#define BME280_OPERATION_MODE_NORMAL	3
+
+// Oversampling definitions
+#define BME280_OVERSAMPLING_1 	1
+#define BME280_OVERSAMPLING_2 	2
+#define BME280_OVERSAMPLING_4 	3
+#define BME280_OVERSAMPLING_8 	4
+#define BME280_OVERSAMPLING_16 	5
+
+
+#define BME280_REGISTER_ADDRESS_CHIP_ID 	0xD0
+#define BME280_REGISTER_ADDRESS_RESET 		0xE0
+#define BME280_REGISTER_ADDRESS_CTRL_HUM 	0xF2
+#define BME280_REGISTER_ADDRESS_STATUS 		0xF3
+#define BME280_REGISTER_ADDRESS_CTRL_MEAS 	0xF4
+#define BME280_REGISTER_ADDRESS_CONFIG		0xF5
+#define BME280_REGISTER_ADDRESS_PRESS_MSB	0xF7
+#define BME280_REGISTER_ADDRESS_PRESS_LSB	0xF8
+#define BME280_REGISTER_ADDRESS_PRESS_XLSB	0xF9
+#define BME280_REGISTER_ADDRESS_TEMP_MSB	0xFA
+#define BME280_REGISTER_ADDRESS_TEMP_LSB	0xFB
+#define BME280_REGISTER_ADDRESS_TEMP_XLSB	0xFC
+#define BME280_REGISTER_ADDRESS_HUM_MSB		0xFD
+#define BME280_REGISTER_ADDRESS_HUM_LSB		0xFE
 
 bme280_handler_t* bme280_init(I2C_HandleTypeDef *i2c_handler);
-bme280_handler_t* bme280_destroy(bme280_handler_t* bme280_handler);
+void bme280_destroy(bme280_handler_t* bme280_handler);
+
+BME280_STATUS BME280_getChipID(bme280_handler_t* bme280_handler, uint8_t* chipID, uint32_t timeout);
+BME280_STATUS BME280_resetChip(bme280_handler_t* bme280_handler, uint32_t timeout);
 
 #endif
--- Src/BME280_Library.c
+++ Src/BME280_Library.c
@@ -2,17 +2,15 @@
 #include "stm32f4xx_hal.h"
 #include "BME280_Library.h"
 
+#include <stdlib.h>
+#include <assert.h>
+
 struct bme280_handler_t
 {
 	I2C_HandleTypeDef *i2c_handler;
 };
 
 /** Functions ----------------------------------------------------------------*/
-uint8_t BME280_getChipID()
-{
-	return 0;
-}
-
 bme280_handler_t* bme280_init(I2C_HandleTypeDef *i2c_handler)
 {
 	bme280_handler_t *bme280 = malloc(sizeof(bme280_handler_t));
@@ -25,10 +23,56 @@ bme280_handler_t* bme280_init(I2C_HandleTypeDef *i2c_handler)
 	return bme280;
 }
 
-bme280_handler_t* bme280_destroy(bme280_handler_t* bme280_handler)
+void bme280_destroy(bme280_handler_t* bme280_handler)
 {
 	if(bme280_handler)
 	{
 		free(bme280_handler);
+	}
+}
+
+BME280_STATUS BME280_getChipID(bme280_handler_t* bme280_handler, uint8_t* chipID, uint32_t timeout)
+{
+	assert(bme280_handler);
+	assert(chipID);
+	assert(timeout > 0);
+
+	HAL_StatusTypeDef retVal;
+	uint8_t chipIDRegisterAddress = BME280_REGISTER_ADDRESS_CHIP_ID;
+	retVal = HAL_I2C_Master_Transmit(bme280_handler->i2c_handler, BME280_DEVICE_ADDRESS, &chipIDRegisterAddress, sizeof(chipIDRegisterAddress), timeout);
+	if(retVal != HAL_OK)
+	{
+		return BME280_FAIL;
+	}
+
+	retVal = HAL_I2C_Master_Receive(bme280_handler->i2c_handler, BME280_DEVICE_ADDRESS, chipID, 1, timeout);
+	if(retVal == HAL_OK)
+	{
+		return BME280_OK;
+	}
+	else
+	{
+		return BME280_FAIL;
+	}
+}
+
+BME280_STATUS BME280_resetChip(bme280_handler_t* bme280_handler, uint32_t timeout)
+{
+	assert(bme280_handler);
+	assert(timeout > 0);
+
+	bme280_register_frame dataFrame;
+	dataFrame.registerAddress = BME280_REGISTER_ADDRESS_RESET;
+	dataFrame.registerData = BME280_RESET_REGISTER_VALUE;
+
+	HAL_StatusTypeDef retVal;
+	retVal = HAL_I2C_Master_Transmit(bme280_handler->i2c_handler, BME280_DEVICE_ADDRESS, (uint8_t*)&dataFrame, sizeof(dataFrame), timeout);
+	if(retVal == HAL_OK)
+	{
+		return BME280_OK;
+	}
+	else
+	{
+		return BME280_FAIL;
 	}
 }
