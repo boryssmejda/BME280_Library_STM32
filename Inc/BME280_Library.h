422cc6e76193c93283cd281ab151fdaec4104b6e
boryssmejda <borys.smejda@gmail.com>
2020-11-02 13:37:40 +0100

    Added compensation coefficients.

--- Inc/BME280_Library.h
+++ Inc/BME280_Library.h
@@ -28,6 +28,23 @@
 #define BME280_OVERSAMPLING_8 	4
 #define BME280_OVERSAMPLING_16 	5
 
+// Table 27 t_sb definitions - t standby
+#define BME280_STANDBY_0_5		0
+#define BME280_STANDBY_62_5		1
+#define BME280_STANDBY_125		2
+#define BME280_STANDBY_250		3
+#define BME280_STANDBY_500		4
+#define BME280_STANDBY_1000		5
+#define BME280_STANDBY_10		6
+#define BME280_STANDBY_20		7
+
+// Table 28 Filter Settings
+#define BME280_FILTER_OFF				0
+#define BME280_FILTER_COEFFICIENT_2		1
+#define BME280_FILTER_COEFFICIENT_4		2
+#define BME280_FILTER_COEFFICIENT_8		3
+#define BME280_FILTER_COEFFICIENT_16	4
+
 
 #define BME280_REGISTER_ADDRESS_CHIP_ID 	0xD0
 #define BME280_REGISTER_ADDRESS_RESET 		0xE0
@@ -139,13 +156,55 @@ typedef struct bme280_control_registers
 	bme280_register_config 		config;
 }bme280_control_registers;
 
+typedef struct bme280_registers_pressure
+{
+	uint8_t registerAddress;
+	bme280_register_press_msb 	pressMsb;
+	bme280_register_press_lsb 	pressLsb;
+	bme280_register_press_xlsb 	pressXlsb;
+}bme280_registers_pressure;
+
+typedef struct
+{
+	bme280_register_temp_msb 	tempMsb;
+	bme280_register_temp_lsb	tempLsb;
+	bme280_register_temp_xlsb	tempXlsb;
+}bme280_registers_temperature;
+
+typedef struct //bme280_registers_humidity
+{
+	uint8_t registerAddress;
+	bme280_register_hum_msb 	humMsb;
+	bme280_register_hum_lsb		humLsb;
+}bme280_registers_humidity;
+
+typedef struct //bme280_registers_compensation_parameters_temperature
+{
+	uint8_t registerAddress;
+
+	uint8_t dig_T1_lsb;
+	uint8_t dig_T1_msb;
+
+	uint8_t dig_T2_lsb;
+	uint8_t dig_T2_msb;
+
+	uint8_t dig_T3_lsb;
+	uint8_t dig_T3_msb;
+}bme280_registers_compensation_parameters_temperature;
+
+
 typedef enum BME280_STATUS BME280_STATUS;
 typedef struct bme280_handler_t bme280_handler_t;
+typedef int32_t BME280_S32_t;
 
 bme280_handler_t* bme280_init(I2C_HandleTypeDef *i2c_handler);
 void bme280_destroy(bme280_handler_t* bme280_handler);
 
 BME280_STATUS BME280_getChipID(bme280_handler_t* bme280_handler, uint8_t* chipID, uint32_t timeout);
 BME280_STATUS BME280_resetChip(bme280_handler_t* bme280_handler, uint32_t timeout);
+
+BME280_STATUS BME280_getTemperatureCompensationParameters(bme280_handler_t* bme280_handler, bme280_registers_compensation_parameters_temperature* compTermp);
+BME280_STATUS BME280_readADCTemperature(bme280_handler_t* bme280_handler, BME280_S32_t *adc_T, uint32_t timeout);
+BME280_S32_t BME280_convertTempIntoReadable(BME280_S32_t adc_T);
 
 #endif
--- Src/BME280_Library.c
+++ Src/BME280_Library.c
@@ -5,6 +5,12 @@
 #include <stdlib.h>
 #include <assert.h>
 
+
+BME280_S32_t t_fine;
+uint16_t dig_T1;
+int16_t dig_T2;
+int16_t dig_T3;
+
 struct bme280_handler_t
 {
 	I2C_HandleTypeDef *i2c_handler;
@@ -75,4 +81,85 @@ BME280_STATUS BME280_resetChip(bme280_handler_t* bme280_handler, uint32_t timeou
 	{
 		return BME280_FAIL;
 	}
+}
+
+BME280_STATUS BME280_getTemperatureCompensationParameters(bme280_handler_t* bme280_handler, bme280_registers_compensation_parameters_temperature* compTermp)
+{
+	int8_t values[6];
+	uint8_t compTempAddress = 0x88;
+
+	HAL_StatusTypeDef retVal;
+	retVal = HAL_I2C_Master_Transmit(bme280_handler->i2c_handler, BME280_DEVICE_ADDRESS, &compTempAddress, sizeof(compTempAddress), 1000);
+	if(retVal != HAL_OK)
+	{
+		return BME280_FAIL;
+	}
+
+	retVal = HAL_I2C_Master_Receive(bme280_handler->i2c_handler, BME280_DEVICE_ADDRESS, values, sizeof(values), 1000);
+	if(retVal != HAL_OK)
+	{
+		return BME280_FAIL;
+	}
+
+	dig_T1 = 0;
+	dig_T1 |= values[1] << 8;
+	dig_T1 |= values[0];
+
+	dig_T2 = 0;
+	dig_T2 |= values[3] << 8;
+	dig_T2 |= values[2];
+
+	dig_T3 = 0;
+	dig_T3 |= values[5];
+	dig_T3 |= values[4];
+
+
+	return BME280_OK;
+}
+
+BME280_STATUS BME280_readADCTemperature(bme280_handler_t* bme280_handler, BME280_S32_t *adc_T, uint32_t timeout)
+{
+	assert(bme280_handler);
+	assert(adc_T);
+
+	bme280_registers_temperature tempReg;
+	tempReg.tempMsb.temp_msb = 0;
+	tempReg.tempLsb.temp_lsb = 0;
+	tempReg.tempXlsb.temp_xlsb = 0;
+
+	uint8_t tempRegisterDeviceAddress = BME280_REGISTER_ADDRESS_TEMP_MSB;
+	HAL_StatusTypeDef retVal;
+	retVal = HAL_I2C_Master_Transmit(bme280_handler->i2c_handler, BME280_DEVICE_ADDRESS, &tempRegisterDeviceAddress, sizeof(tempRegisterDeviceAddress), timeout);
+
+	if(retVal != HAL_OK)
+	{
+		return BME280_FAIL;
+	}
+
+	retVal = HAL_I2C_Master_Receive(bme280_handler->i2c_handler, BME280_DEVICE_ADDRESS, (uint8_t*)&tempReg, sizeof(tempReg), timeout);
+
+	if(retVal != HAL_OK)
+	{
+		return BME280_FAIL;
+	}
+	else
+	{
+		*adc_T = 0;
+		*adc_T |= tempReg.tempMsb.temp_msb << 16;
+		*adc_T |= tempReg.tempLsb.temp_lsb << 8;
+		*adc_T |= tempReg.tempXlsb.temp_xlsb;
+		return BME280_OK;
+	}
+}
+
+BME280_S32_t BME280_convertTempIntoReadable(BME280_S32_t adc_T)
+{
+	BME280_S32_t var1, var2, T;
+	var1 = ( (( (adc_T>>3) - ( (BME280_S32_t)dig_T1<<1) ) ) * ((BME280_S32_t)dig_T2)) >> 11;
+
+	var2 = (((((adc_T>>4) - ((BME280_S32_t)dig_T1)) * ((adc_T>>4) - ((BME280_S32_t)dig_T1)))>> 12) * ((BME280_S32_t)dig_T3)) >> 14;
+
+	t_fine = var1 + var2;
+	T = (t_fine * 5 + 128) >> 8;
+	return T;
 }
